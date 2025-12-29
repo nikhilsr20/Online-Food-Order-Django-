45770b809b54a717f627efd314e3075c3c4f5a5e
Nikhil singh Rathore <nikhils1620@gmail.com>
2025-12-30 00:19:12 +0530

    day-60

--- main/restaurantpage/views.py
+++ main/restaurantpage/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from partner.models import Restaurants,Item
 # Create your views here.
 
-def restaurant(request):
-    return render(request,'restaurantpage/res.html')
+def restaurant(request,id):
+    # restaurant=Restaurants.objects.get(id=id)
+
+    restaurant=get_object_or_404(Restaurants,id=id)
+    print(restaurant)
+
+    category=restaurant.categories.all()
+
+    items=Item.objects.filter(category__restaurant=restaurant)
+
+    return render(request,'restaurantpage/res.html',{'category':category,'items':items,'restaurant':restaurant})
 
